ff992badb25849ebc57fd72168014b7e201379cb
RKulkarniSE <raj.kulkarni@siemens-energy.com>
2023-11-07 14:11:42 +1100

    inital commit of KPI page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import PlantDataTables from './Routes/PlantDataTables';
 import Footprints from './Routes/FootPrints';
+import KPIs from './Routes/KPIs';
 import './styles.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<PlantDataTables/>}/>
           <Route exact path='/FOOTPRINTS' element={<Footprints/>}/>
+          <Route exact path='/KPI' element={<KPIs/>}/>
         </Routes>
           
       </BrowserRouter>
---
+++ src/Routes/KPIs.js
@@ -0,0 +1,9 @@
+
+export default function KPIs(){
+
+    return(
+        <div>
+            Sup dude
+        </div>
+    )
+}
